d2c9039a23f688060c3a66de76015992a3ce9c9d
piers.piers <piercelaxamana@gmail.com>
2026-09-11 05:24:57 +0000

    Created Flutter App name demo

---
+++ demo/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World', style: TextStyle(
+              fontSize: 50,
+              color: Colors.pink
+            )
+
+          ),
+
+        ),
+      ),  
+
+
+
+    );
+
+
+  }
+}
